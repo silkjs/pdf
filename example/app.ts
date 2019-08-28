bcba167b7ccdc984fcbe30caccc3ba95add88c6f
g.o <g.o@shengdong.local>
2019-08-28 15:33:07 +0800

    update

--- example/app.ts
+++ example/app.ts
@@ -1,13 +1,12 @@
-import * as pkg from '../';
-console.log(pkg);
-// makePDF(doc => {
-//   doc.text('dsfs');
-//   doc.end();
-// })
-//   .then(url => {
-//     console.log(url);
-//     document.querySelector('iframe').src = url;
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+import { makePDF } from '../';
+
+makePDF(doc => {
+  doc.text('dsfs');
+  doc.end();
+})
+  .then(url => {
+    document.querySelector('iframe').src = url;
+  })
+  .catch(err => {
+    console.log(err);
+  });
